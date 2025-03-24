907b49d6b79cebc979602fd406828ad03c931d41
rutvik7111 <marwadi.rutvik711@gmail.com>
2025-03-25 00:39:40 +0530

    fixed issues again 2

--- app/SuccessfulPayment/page.js
+++ app/SuccessfulPayment/page.js
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import { Suspense } from 'react'
 import Success from '../components/Success'
@@ -5,7 +6,7 @@ import Success from '../components/Success'
 const page = () => {
     return (
         <Suspense>
-            rutvik
+            <Success />
         </Suspense>
     )
 }
