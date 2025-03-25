8739b0c7cb54e4a8285fad2bedfe5d544509d8a3
rutvik7111 <marwadi.rutvik711@gmail.com>
2025-03-25 12:48:04 +0530

    modified razorpay.js to handle error

--- app/razorpay/razorpay.js
+++ app/razorpay/razorpay.js
@@ -8,26 +8,30 @@ export default async function createOrder(amount, fromUser, toUser, message) {
     await mongoose.connect(process.env.MONGODB_URI)
     const secret = await RazorpaySecret.findOne({ userId: toUser.userId })
 
-    var instance = new Razorpay({ key_id: toUser.razorpayId, key_secret: secret.razorpaySecret })
+    try {
+        var instance = new Razorpay({ key_id: toUser.razorpayId, key_secret: secret.razorpaySecret })
 
-    const order = await instance.orders.create({
-        amount: `${amount}`,
-        currency: "INR",
-        receipt: "receipt#1",
-        notes: {
-            key1: "value3",
-            key2: "value2"
-        }
-    })
+        const order = await instance.orders.create({
+            amount: `${amount}`,
+            currency: "INR",
+            receipt: "receipt#1",
+            notes: {
+                key1: "value3",
+                key2: "value2"
+            }
+        })
 
-    const payment = await Payments.create({
-        orderId: order.id,
-        amount: amount / 100,
-        from_user: fromUser.id,
-        to_user: toUser.userId,
-        name: fromUser.name,
-        message: message
-    })
+        const payment = await Payments.create({
+            orderId: order.id,
+            amount: amount / 100,
+            from_user: fromUser.id,
+            to_user: toUser.userId,
+            name: fromUser.name,
+            message: message
+        })
+    } catch (error) {
+        console.log(error);
+    }
 
     return order;
 }
